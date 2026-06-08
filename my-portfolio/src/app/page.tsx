f36ef72b64fe2b337a88a3f4db929260763b083a
Channa2146@gmail.com <Channa2146@gmail.com>
2026-06-08 10:56:07 +0530

    mock code

--- my-portfolio/src/app/page.tsx
+++ my-portfolio/src/app/page.tsx
@@ -1,8 +1,15 @@
 import Image from "next/image";
+import Header from "./components/default/Header";
+import Footer from "./components/default/Footer";
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
     <section className="flex flex-col w-full h-full">
+      <Header />
+      <Hero />
+
+      <Footer />
       
 
     </section>
