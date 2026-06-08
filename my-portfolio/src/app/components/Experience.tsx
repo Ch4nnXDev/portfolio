1ce8409ca96570979ba8c9b6a54d7beff2a92311
Channa2146@gmail.com <Channa2146@gmail.com>
2026-06-08 10:56:17 +0530

    mock code

---
+++ my-portfolio/src/app/components/Experience.tsx
@@ -0,0 +1,7 @@
+export default function Experience() {
+    return (
+        <section className="flex flex-col p-4 w-full">
+            <h2 className="text-3xl font-bold mb-4">Experience</h2>
+        </section>
+    );
+}
