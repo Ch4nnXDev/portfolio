0fe4c5553c8d237b0736a8c6270ac4aba0654423
Channa2146@gmail.com <Channa2146@gmail.com>
2026-06-08 10:56:44 +0530

    mock code

---
+++ my-portfolio/src/app/components/Skills.tsx
@@ -0,0 +1,7 @@
+export default function Skills() {
+    return (
+        <section className="flex flex-col p-4 w-full">
+            <h2 className="text-3xl font-bold mb-4">Skills</h2>
+        </section>
+    );
+}
