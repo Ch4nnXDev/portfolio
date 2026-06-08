db6f7c6bb057e575d941eb2ed921eeee21676490
Channa2146@gmail.com <Channa2146@gmail.com>
2026-06-08 10:56:30 +0530

    mock code

---
+++ my-portfolio/src/app/components/Hero.tsx
@@ -0,0 +1,8 @@
+export default function Hero() {
+    return (
+        <section className="flex flex-col p-4 w-full">
+            <h1 className="text-4xl font-bold">Welcome to My Portfolio</h1>
+            <p className="text-lg">I'm a passionate developer creating amazing web experiences.</p>
+        </section>
+    );
+}
