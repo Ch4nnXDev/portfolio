75242c4fd3994df0377bafeb5e5c3fd41e4ab20e
Channa2146@gmail.com <Channa2146@gmail.com>
2026-06-09 11:25:53 +0530

    changed section tag to header tag

--- my-portfolio/src/app/my-components/default/Header.tsx
+++ my-portfolio/src/app/my-components/default/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
-        <section className="flex flex-col p-4 w-full bg-white text-black">
-            <nav className="flex flex-row items-center">
+        <header>
+            <nav className="flex flex-row items-center bg-white/5 backdrop-blur-sm z-10">
                 <ul className="flex space-x-4  justify-between gap-10">
                     <li><a href="#hero" className="text-lg font-medium">Home</a></li>
                     <li><a href="#projects" className="text-lg font-medium">Projects</a></li>
@@ -9,6 +9,6 @@ export default function Header() {
                     <li><a href="#experience" className="text-lg font-medium">Experience</a></li>
                 </ul>
             </nav>
-        </section>
+        </header>
     )
 }
