87cfbdf53e4cca3a248f1f96ebb8a66786263b43
Channa2146@gmail.com <Channa2146@gmail.com>
2026-06-09 10:32:45 +0530

    fix

--- my-portfolio/src/app/my-components/Hero.tsx
+++ my-portfolio/src/app/my-components/Hero.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
             <Vortex  className="flex flex-col w-full items-center justify-center h-full">
                 <div className="flex flex-col w-full">
                     <Image src="/profile.jpeg" alt="Profile Picture" width={150} height={150} className="rounded-full mb-4 self-center" />
-                    <h1 className="text-5xl font-bold w-full">Channa Karawita</h1>
+                    <h1 className="text-5xl w-full">Channa Karawita</h1>
                     <p className="text-xl w-full">Software Engineer | Full Stack Developer</p>
                 </div>
 
