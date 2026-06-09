16131c65e5136b06d7191f66f930713643d5fae3
Channa2146@gmail.com <Channa2146@gmail.com>
2026-06-09 09:35:03 +0530

    added state into projects component

--- my-portfolio/src/app/my-components/Projects.tsx
+++ my-portfolio/src/app/my-components/Projects.tsx
@@ -33,6 +33,8 @@ export default function Projects() {
     return (
         <section className="flex flex-col p-4 w-full">
             <h2 className="text-3xl font-bold mb-4">Projects</h2>
+            <div></div>
+
         </section>
     );
 }
