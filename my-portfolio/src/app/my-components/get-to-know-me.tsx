de03b01eedc96876174e1ba84c26336bfaacd990
Channa2146@gmail.com <Channa2146@gmail.com>
2026-06-10 10:33:55 +0530

    added new component

---
+++ my-portfolio/src/app/my-components/get-to-know-me.tsx
@@ -0,0 +1,26 @@
+export default function Me() {
+    return (
+        <section className="flex flex-col">
+            <div className="flex flex-col">
+                <h1 className="font-medium">
+                    Get to Know Me
+                </h1>
+                <h2 className="font-medium">
+                    Current Standing 
+                </h2>
+            </div>
+            <div className="">
+                <p className="text-black text-lg">
+                    had my secondary Education from Anuradhapura Central College Attended A/Levels in Biology Science Stream.
+                    Currently a Final Year Student of Informatics Institue of Technology, Sri lanka which is a affiliation of University of Westminster.
+                    During My Year i have commited into AI Model Architecture and Backend Engineering which involves Network Engineering and Distributed Architecture.
+                    I have a Well Rounded Knowledge on System Design and Infra. During my Industrial Placement Year I Worked as a Software Engineer Intern
+                    at SLT Head Office. After Finishing My Internship i had more time before my Final Year Starts During this period i got the opportunity to
+                    work a double role as a Web Developer and a Digital Marketing Executive this role is more aligned toward Growth Engineering thus it has given me business knwoledge
+                    of how businesses digitally scale and how to perform data analysis and come to correct engineering and marketing decisions.
+                </p>
+            </div>
+
+        </section>
+    );
+}
