808df4d113a4bced06c951f888adce9fefe7ce8c
Channa2146@gmail.com <Channa2146@gmail.com>
2026-06-10 10:53:09 +0530

    added the Me Component

--- my-portfolio/src/app/page.tsx
+++ my-portfolio/src/app/page.tsx
@@ -1,4 +1,4 @@
-
+import Me from "./my-components/get-to-know-me"
 import Hero from "./my-components/Hero";
 import Skills from "./my-components/Skills";
 import Experience from "./my-components/Experience";
@@ -8,6 +8,7 @@ export default function Home() {
   return (
     <main className="min-h-screen">
       <Hero />
+      <Me />
       <Skills />
       <Experience />
       <Projects />
