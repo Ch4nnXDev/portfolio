b7949964c716e851df5031560dffc4d43d58856c
Channa2146@gmail.com <Channa2146@gmail.com>
2026-06-10 10:34:20 +0530

    added layout fixes in the Hero

--- my-portfolio/src/app/my-components/Hero.tsx
+++ my-portfolio/src/app/my-components/Hero.tsx
@@ -1,13 +1,28 @@
-import { Vortex } from "@/components/ui/vortex";
 import Image from "next/image";
 export default function Hero() {
     return (
-        <section className="flex flex-col p-4 w-full h-screen text-black">
+        <section className="flex flex-col p-4 w-full h-screen text-black items-center justify-center gap-20">
+            <div className="flex flex-col items-center">
+                <Image src="/profile.jpeg" alt="Profile Picture" width={300} height={400} className="rounded-full mb-4 self-center" />
+            </div>
             
-            <div className="flex flex-col w-full">
-                <Image src="/profile.jpeg" alt="Profile Picture" width={150} height={150} className="rounded-full mb-4 self-center" />
-                <h1 className="text-5xl w-full">Channa Karawita</h1>
-                <p className="text-xl w-full">Software Engineer | Full Stack Developer</p>
+            <div className="flex flex-col w-full items-center gap-5">
+                
+                <h1 className="text-5xl ">Channa Karawita</h1>
+                <p className="text-xl">Software Engineer | Growth Engineer | Full Stack Developer | Ai & ML Engineer</p>
+            </div>
+            <div className="flex flex-row p-6">
+                <div className="">
+                    <button></button>
+                </div>
+                <div className="">
+                    <button></button>
+                </div>
+                <div className="">
+                    <button></button>
+                </div>
+            
+
             </div>
 
             
