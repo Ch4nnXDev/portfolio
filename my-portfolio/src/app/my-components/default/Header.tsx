a9f88ee06c35e4f9e8a9f1d341c5a7480b83abf3
Channa2146@gmail.com <Channa2146@gmail.com>
2026-06-10 10:34:34 +0530

    added fixed on Ui and Layout

--- my-portfolio/src/app/my-components/default/Header.tsx
+++ my-portfolio/src/app/my-components/default/Header.tsx
@@ -1,14 +1,14 @@
 export default function Header() {
-    return (
-        <header className="mt-20 border">
-            <nav className="flex items-center gap-10 bg-white/10 backdrop-blur-xl border border-white/20 shadow-xl rounded-full  px-8 py-4">
-                <ul className="flex space-x-4  justify-between gap-10 mt-5 items-center justify-center">
-                    <li><a href="#hero" className="text-lg font-medium">Home</a></li>
-                    <li><a href="#projects" className="text-lg font-medium">Projects</a></li>
-                    <li><a href="#skills" className="text-lg font-medium">Skills</a></li>
-                    <li><a href="#experience" className="text-lg font-medium">Experience</a></li>
-                </ul>
-            </nav>
-        </header>
-    )
+  return (
+    <header className="fixed top-5 left-20 translate-x-1/2">
+      <nav className="flex items-center gap-10 bg-transparent backdrop-blur-xl border border-white/20 shadow-xl rounded-full px-30 py-8">
+        <ul className="flex items-center gap-10">
+          <li><a href="#hero" className="text-xl font-semibold hover:text-blue-800">Home</a></li>
+          <li><a href="#projects" className="text-xl font-semibold hover:text-blue-800">Projects</a></li>
+          <li><a href="#skills" className="text-xl font-semibold hover:text-blue-800">Skills</a></li>
+          <li><a href="#experience" className="text-xl font-semibold hover:text-blue-800">Experience</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
