05e9f3235b6ff7bd946d9fefaf9ab0df5b16cf0e
Channa2146@gmail.com <Channa2146@gmail.com>
2026-06-10 14:22:22 +0530

    removed the h-screen because it makes layout issues

--- my-portfolio/src/app/my-components/Skills.tsx
+++ my-portfolio/src/app/my-components/Skills.tsx
@@ -23,7 +23,7 @@ export default function Skills() {
      
     ]
     return (
-        <section className="flex flex-col p-4 w-full items-center h-[50%]">
+        <section className="flex flex-col p-4 w-full items-center">
             <h2 className="text-3xl font-bold mb-4">Skills</h2>
             <div className="flex flex-row gap-6 items-center">
                 {skills.map((skill, index) => (
