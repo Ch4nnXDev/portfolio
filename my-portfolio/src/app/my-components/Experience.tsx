92ec9f9333543e8cd8604231b770814875e0551b
Channa2146@gmail.com <Channa2146@gmail.com>
2026-06-26 17:36:25 +0530

    fix

--- my-portfolio/src/app/my-components/Experience.tsx
+++ my-portfolio/src/app/my-components/Experience.tsx
@@ -4,7 +4,11 @@ export default function Experience() {
             title: "Software Engineer Intern",
             company: "SLT Headquarters",
             duration: "June 16 2025 - May 16 2026",
-            projects: "",
+            projects: [
+                {
+                    project_name: "FCMS - Fraud Call Management System"
+                }
+            ],
             description: ""
             
         },
@@ -12,7 +16,7 @@ export default function Experience() {
             title: "In-house Web Developer and Digital Marketing Executive",
             company: "Davinci Ceramics",
             duration: "Febraury 2026 - Present",
-            projects: "",
+            projects: [],
             description: ""
         }
 
@@ -22,7 +26,7 @@ export default function Experience() {
             <h2 className="text-3xl font-bold mb-4">Experience</h2>
             <div className="flex flex-col gap-20 ">
                 {expeiences.map((experience, index) => (
-                    <div key={index} className="flex flex-row p-4 bg-gray-100 rounded-lg p-30">
+                    <div key={index} className="flex flex-row p-4 border-sm  gap-20 rounded-lg p-30">
                         <h3 className="text-xl font-semibold">{experience.title}</h3>
                         <p className="text-gray-600">{experience.company}</p>
                         <p className="text-gray-500 text-sm">{experience.duration}</p>
