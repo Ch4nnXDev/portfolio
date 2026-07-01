4d1eb15da506459f1caf80201be2337c597c6f3f
Channa2146@gmail.com <Channa2146@gmail.com>
2026-07-01 11:35:19 +0530

    ok

--- my-portfolio/src/app/my-components/Experience.tsx
+++ my-portfolio/src/app/my-components/Experience.tsx
@@ -4,24 +4,50 @@ import gsap from "gsap";
 
 export default function Experience() {
 
+    const experience = [
+        {
+            position: "Software Engineer Intern",
+            company: "Sri Lanka Telecom",
+            location: "SLT Head Office",
+            duration: "2025 June 16 - 2026 May 16",
+            skills: ""
+        },
+        
+    ]
+
     const boxRef = useRef(null);
 
 
     useEffect(() => {
         
-        gsap.to(boxRef.current, {
-            x:500,
-            duration: 50,
-            rotation: 360
-
-        })
-
+        
     }, []);
     return (
-        <div className="flex flex row justify-center items-center p-20">
-            <div ref={boxRef} className="p-40 mt-10 bg-gray-400 rounded-lg w-full"></div>
+        <section className="p-10 h-screen items-center justify-center">
+            <h2 className="p-20 text-3xl">Experience</h2>
+            {experience.map((exp) => (
+                <div className="grid grid-cols-4 shadow-md items-center gap-1 m-2 rounded-xl shadow-lg rounded-lg" key={exp.position}>
+                
+            
+                    <div ref={boxRef} className="p-20 border rounded-lg col-span-2">{exp.position}</div>
+                    <div className="p-20 border rounded-lg col-span-2">{exp.duration}</div>
+
             
             
-        </div>
+                    <div className="p-60 border rounded-lg col-span-2">{exp.location}</div>
+                    <div className=" border rounded-lg p-60 row-start-2 col-span-2">{exp.company}</div>
+            
+        
+
+            
+            
+                </div>
+
+                ))}
+            
+            
+
+        </section>
+        
     );
 }
